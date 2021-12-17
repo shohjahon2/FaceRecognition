2ca9758aa9247c052993d75b36dd54741561259c
ALI <kradunmaker@gmail.com>
2021-12-17 06:30:01 -0500

    wqwerqerqer

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace FaceRecognition
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// salomalar
         [STAThread]
         static void Main()
         {
