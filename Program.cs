b0ef49a672bc4c17eee0d76c557281f9b86a33f6
ALI <kradunmaker@gmail.com>
2021-12-17 06:35:50 -0500

    Removed salomlar

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace FaceRecognition
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        /// salomalar
         [STAThread]
         static void Main()
         {
